f707af1d5776434540a059df9addeb7ff4bae046
Vítor Marçal <codehero@marcal.dev>
2023-09-22 18:35:51 -0300

    avoid transversal attack during file extraction and storage in the target directory

--- domain/src/main/kotlin/dev/marcal/chatvault/model/BucketFile.kt
+++ domain/src/main/kotlin/dev/marcal/chatvault/model/BucketFile.kt
@@ -1,6 +1,7 @@
 package dev.marcal.chatvault.model
 
 import java.io.File
+import java.lang.IllegalStateException
 
 class BucketFile(
     val bytes: ByteArray,
@@ -10,6 +11,14 @@ class BucketFile(
 
 
     fun file(root: String = "/"): File {
-        return File(root + address.path + fileName)
+        return File(root + address.path + fileName).apply {
+            if (!this.toPath().normalize().startsWith(File(root).toPath())) {
+                throw IllegalStateException("bad file path!out of root directory")
+            }
+
+            if (!this.toPath().normalize().startsWith(File(root + address.path).toPath())) {
+                throw IllegalStateException("bad file path!out of bucket chat directory")
+            }
+        }
     }
 }
