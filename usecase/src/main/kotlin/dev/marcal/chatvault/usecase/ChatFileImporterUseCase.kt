5841338ac59c608f595a34bb4cb5ec9e221fbc3d
Vítor Marçal <codehero@marcal.dev>
2023-09-22 18:20:40 -0300

    feature add chat by zip file

--- usecase/src/main/kotlin/dev/marcal/chatvault/usecase/ChatFileImporterUseCase.kt
+++ usecase/src/main/kotlin/dev/marcal/chatvault/usecase/ChatFileImporterUseCase.kt
@@ -1,42 +1,73 @@
 package dev.marcal.chatvault.usecase
 
-import dev.marcal.chatvault.in_out_boundary.input.NewAttachmentInput
-import dev.marcal.chatvault.in_out_boundary.input.NewMessageInput
+import dev.marcal.chatvault.app_service.bucket_service.BucketService
 import dev.marcal.chatvault.in_out_boundary.input.NewMessagePayloadInput
+import dev.marcal.chatvault.model.BucketFile
+import dev.marcal.chatvault.repository.ChatRepository
 import dev.marcal.chatvault.service.ChatFileImporter
 import dev.marcal.chatvault.service.ChatMessageParser
 import dev.marcal.chatvault.service.MessageCreator
+import dev.marcal.chatvault.usecase.mapper.toNewMessageInput
 import org.springframework.stereotype.Service
+import java.io.BufferedInputStream
+import java.io.ByteArrayInputStream
 import java.io.InputStream
+import java.lang.IllegalStateException
+import java.util.zip.ZipInputStream
 
 @Service
 class ChatFileImporterUseCase(
     private val chatMessageParser: ChatMessageParser,
-    private val messageCreator: MessageCreator
+    private val messageCreator: MessageCreator,
+    private val bucketService: BucketService,
+    private val chatRepository: ChatRepository
 ) : ChatFileImporter {
+
+    private val possiblyWhatsappTalk = Regex(".*WhatsApp.*\\.txt$")
     override fun execute(chatId: Long, inputStream: InputStream, fileType: String) {
-        when(fileType) {
+        when (fileType) {
             "zip" -> {
-                TODO("$fileType parser not implemented yet")
+                iterateOverZip(chatId, inputStream)
             }
+
             "text" -> {
                 createMessages(inputStream = inputStream, chatId = chatId)
             }
-            else -> throw RuntimeException("file type $fileType not supported")
+
+            else -> throw IllegalStateException("file type $fileType not supported")
         }
 
 
     }
 
+    private fun iterateOverZip(chatId: Long, inputStream: InputStream) {
+        val chatBucketInfo =
+            chatRepository.findChatBucketInfoByChatId(chatId = chatId) ?: throw RuntimeException("chatId not found")
+        val bucket = chatBucketInfo.bucket.withPath("/")
+
+        val zipInputStream = ZipInputStream(BufferedInputStream(inputStream))
+
+        var entry = zipInputStream.nextEntry
+        while (entry != null) {
+            val fileName = entry.name
+
+            val buffer = ByteArray(1024)
+            zipInputStream.read(buffer)
+
+            bucketService.save(BucketFile(bytes = buffer, fileName = fileName, address = bucket))
+
+            if (possiblyWhatsappTalk.find(fileName) != null) {
+                execute(chatId = chatId, inputStream = ByteArrayInputStream(buffer), fileType = "text")
+            }
+
+            entry = zipInputStream.nextEntry
+        }
+        zipInputStream.close()
+    }
+
     private fun createMessages(inputStream: InputStream, chatId: Long) {
         val messages = chatMessageParser.parseAndTransform(inputStream) { messageOutput ->
-            NewMessageInput(
-                authorName = messageOutput.author ?: "",
-                chatId = chatId,
-                createdAt = messageOutput.createdAt,
-                content = messageOutput.content,
-                attachment = messageOutput.attachmentName?.let { NewAttachmentInput(name = it, content = "") }
-            )
+            messageOutput.toNewMessageInput(chatId = chatId)
         }
 
         messageCreator.execute(
