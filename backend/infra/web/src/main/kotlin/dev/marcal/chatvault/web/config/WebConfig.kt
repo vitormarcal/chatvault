a04382795c8ab81c01773b39ed0ced5ab2892159
Vítor Marçal <codehero@marcal.dev>
2023-10-18 21:35:35 -0300

    enable cors

---
+++ backend/infra/web/src/main/kotlin/dev/marcal/chatvault/web/config/WebConfig.kt
@@ -0,0 +1,21 @@
+package dev.marcal.chatvault.web.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class WebConfig {
+
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000")
+            }
+        }
+    }
+}
