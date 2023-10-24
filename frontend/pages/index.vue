fce98b8b3007fea78033cc288ad84c0795c75d69
Vítor Marçal <codehero@marcal.dev>
2023-10-24 12:00:55 -0300

    toggle view message area

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -6,9 +6,14 @@
     <main class="container-fluid">
       <div class="row h-100">
         <chat-list :chats="chats"
+                   :active-chat="chat"
+                   :mobile="isMobile"
                    @update:chat-active="updateChatActive"
         />
-        <message-area :chat="chat"/>
+        <message-area
+            :mobile="isMobile"
+            @update:chat-exited="updateChatExited"
+            :chat="chat"/>
       </div>
     </main>
 
@@ -22,10 +27,26 @@ import MessageArea from "~/components/MessageArea.vue";
 const listChatsAPIUrl = useRuntimeConfig().public.api.listChats
 const {data: chats} = await useFetch(listChatsAPIUrl)
 const chat = ref({})
+const isMobile = ref(true)
+
+function checkWindowSize() {
+  isMobile.value = window.innerWidth <= 575;
+}
 
 function updateChatActive(item: any) {
   chat.value = item
 }
+
+function updateChatExited() {
+  chat.value = {}
+}
+
+onMounted(() => {
+  nextTick(() => {
+    window.addEventListener('resize', checkWindowSize);
+  })
+})
+
 
 </script>
 
