a01808de32b3fd8eb41e7fbd57063d9efd7ff0fc
Vítor Marçal <codehero@marcal.dev>
2023-10-26 17:14:27 -0300

    with ssr:true the frontend build for some reason is not making requests

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,7 +3,7 @@
 const host = process.env.API_HOST || '/api'
 export default defineNuxtConfig({
     devtools: {enabled: true},
-    ssr: true,
+    ssr: false,
     runtimeConfig: {
         public: {
             api: {
