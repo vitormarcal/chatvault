08ab781a2e7b3207fffd857970d2d904a09e2910
Vitor Marçal <codehero@marcal.dev>
2023-10-26 16:38:36 -0300

    34 import chat via frontend (#45)
    * refactoring name
    
    * feat chat upload to server
    
    * update ignore
    
    * update docker
    
    * update docker
    
    * ssr: true
    
    * upload completed

--- frontend/components/MessageArea.vue
+++ frontend/components/MessageArea.vue
@@ -28,10 +28,16 @@
 
       </div>
 
-      <div class="chat-option d-flex mt-3">
+      <div class="chat-option mt-3 ">
+
+
+        <button type="button" @click="toggleModal" class="btn btn-outline-primary mb-2" data-bs-toggle="modal">
+          Import/Export
+        </button>
+
         <div class="form-group">
-          <label for="exampleFormControlSelect1">Active Author</label>
-          <select class="form-control" v-model="authorActive" id="exampleFormControlSelect1">
+          <label for="active-author">Active Author</label>
+          <select class="form-control" v-model="authorActive" id="active-author">
             <option v-for="option in authors" :value="option">{{ option }}</option>
           </select>
         </div>
@@ -39,14 +45,58 @@
 
 
     </div>
-
-
     <div id="infinite-list" class="message-list d-flex flex-column">
       <button v-if="hasNextPages" type="button" class="btn btn-light" @click="loadMoreMessages">Load more messages
       </button>
       <template v-for="(message, index) in messages" :key="index">
         <message-item :message="message" :chatId="chat.chatId" :authorActive="authorActive"/>
       </template>
+    </div>
+
+    <div class="modal" :class="modalClass" tabindex="-1">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title text-black">File importer/exporter</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" @click="toggleModal"
+                    aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="form-control">
+              <div class="alert alert-success" v-if="importChatResult.data" role="alert">
+                {{ importChatResult.data }}
+              </div>
+              <div class="alert alert-warning" v-if="importChatResult.error" role="alert">
+                Failed to import, check server error logs
+              </div>
+              <div class="form-group d-flex justify-content-center mb-3">
+                <button type="button" class="btn btn-link btn-outline-info">Get the entire chat</button>
+              </div>
+
+              <div class="form-group mb-3 pt-4 border-top border-2">
+                <p class="text-black">Attention, these actions are related to the selected chat! </p>
+                <label for="formFileSm" class="form-label text-black">Import messages to this chat</label>
+                <input class="form-control form-control-sm"
+                       @change="onFilePicked"
+
+                       id="formFileSm"
+                       ref="chatImportRef"
+                       type="file">
+              </div>
+              <div class="btn-group" role="group">
+                <button type="button" :disabled="disableUpload" @click="uploadFile"
+                        class="btn btn-outline-secondary ml-2">Upload
+                </button>
+              </div>
+            </div>
+
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" @click="toggleModal" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
     </div>
 
 
@@ -63,17 +113,19 @@ const messages = ref([])
 const authorActive = ref({})
 const nextPage = ref(0)
 const messagesAreaElement = ref(null)
+const chatImportRef = ref(null)
+const clickModal = ref(false)
+const importChatResult = ref({})
+const disableUpload = ref(true)
 
 const chatActive = computed(() => props.chat.chatId > 0)
 
 const moreMessagesPath = computed(() =>
     useRuntimeConfig().public.api.getMessagesByIdAndPage.replace(":chatId", props.chat.chatId).replace(":page", nextPage.value))
 
+const importChatPath = computed(() => useRuntimeConfig().public.api.importChatById.replace(":chatId", props.chat.chatId))
 
-const {data: response} = await useLazyFetch(() => moreMessagesPath.value, {
-  immediate: false
-})
-
+const {data: response, refresh} = await useFetch(moreMessagesPath)
 
 const content = computed(() => {
   return response?.value?.content ?? []
@@ -81,6 +133,12 @@ const content = computed(() => {
 
 const authors = computed(() => {
   return [...new Set(messages.value.map(it => it.author))].filter(it => !!it)
+})
+
+const modalClass = computed(() => {
+  return {
+    'fade show d-block': !!clickModal.value
+  }
 })
 
 const hasNextPages = computed(() => {
@@ -96,6 +154,15 @@ const dynamicClass = computed(() => {
     'd-none': props.mobile && props.chat.chatId == null
   }
 })
+
+
+function toggleModal() {
+  clickModal.value = !clickModal.value
+  importChatResult.value = {}
+  if (chatImportRef.value) {
+    chatImportRef.value.value = ''
+  }
+}
 
 function scrollBottom() {
   if (messagesAreaElement.value && nextPage.value === 0) {
@@ -114,11 +181,40 @@ function exitThisChat() {
   emit('update:chat-exited')
 }
 
+async function uploadFile() {
+  if (chatImportRef?.value?.files && chatImportRef?.value?.files[0]) {
+    let input = chatImportRef.value;
+    const file = input.files[0]
+    console.log(file)
+
+    const form = new FormData()
+    form.append("file", file);
+    importChatResult.value = await useAsyncData(`upload ${file.name}`, () => $fetch(importChatPath.value, {
+      method: "POST",
+      body: form
+    }))
+    chatImportRef.value.value = ''
+    if (importChatResult.value.data) {
+      await refresh()
+    }
+  }
+}
+
+async function onFilePicked() {
+  if (chatImportRef?.value?.files && chatImportRef?.value?.files[0]) {
+    disableUpload.value = false
+  }
+}
+
 watch(
     () => props.chat.chatId,
     (chatId) => {
       messages.value = []
       nextPage.value = 0
+      disableUpload.value = true
+      if (chatImportRef.value) {
+        chatImportRef.value.value = ''
+      }
     }
 )
 
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,13 +3,14 @@
 const host = process.env.API_HOST || '/api'
 export default defineNuxtConfig({
     devtools: {enabled: true},
-    ssr: false,
+    ssr: true,
     runtimeConfig: {
         public: {
             api: {
                 listChats: `${host}/chats`,
                 getMessagesByIdAndPage: `${host}/chats/:chatId?page=:page`,
-                getAttachmentByChatIdAndMessageId: `${host}/chats/:chatId/messages/:messageId/attachment`
+                getAttachmentByChatIdAndMessageId: `${host}/chats/:chatId/messages/:messageId/attachment`,
+                importChatById: `${host}/chats/:chatId/messages/import`
             }
         }
     },
