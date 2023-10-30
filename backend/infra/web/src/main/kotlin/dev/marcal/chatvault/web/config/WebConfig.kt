c812007924910318a5045291701e8ff9657e3bfc
Vítor Marçal <codehero@marcal.dev>
2023-10-30 19:47:34 -0300

    allow  cors

--- backend/infra/web/src/main/kotlin/dev/marcal/chatvault/web/config/WebConfig.kt
+++ backend/infra/web/src/main/kotlin/dev/marcal/chatvault/web/config/WebConfig.kt
@@ -1,20 +1,24 @@
 package dev.marcal.chatvault.web.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-
+@PropertySource("classpath:web.properties")
 @Configuration
-class WebConfig {
+class WebConfig(
+    @Value("\${chatvault.host}") private val allowedOrigin: List<String>
+) {
 
 
     @Bean
     fun corsConfigurer(): WebMvcConfigurer {
         return object : WebMvcConfigurer {
             override fun addCorsMappings(registry: CorsRegistry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000")
+                registry.addMapping("/**").allowedOrigins(*allowedOrigin.toTypedArray())
             }
         }
     }
