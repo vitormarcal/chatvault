4bf2bafd2f0da19054cb2d7e987ff7988223d268
Vítor Marçal <codehero@marcal.dev>
2023-11-08 09:45:44 -0300

    create components to svg icons

--- frontend/components/ChatConfig.vue
+++ frontend/components/ChatConfig.vue
@@ -39,14 +39,8 @@ function validatedPageSize(event: any) {
       <div class="input-group">
 
       <span class="input-group-text" @click="toggleChatName()">
-        <svg xmlns="http://www.w3.org/2000/svg" v-if="editChatName" width="16" height="16" fill="currentColor"
-             class="bi bi-check2" viewBox="0 0 16 16"> <path
-            d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/></svg>
-        <svg xmlns="http://www.w3.org/2000/svg" v-else width="16" height="16" fill="currentColor"
-             class="bi bi-pencil-square" viewBox="0 0 16 16"><path
-            d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"></path><path
-            fill-rule="evenodd"
-            d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"></path></svg>
+        <icon-check v-if="editChatName" />
+        <icon-pencil-square v-else />
       </span>
         <input type="text" :disabled="!editChatName" id="chatname-inpiut" class="form-control" v-model="chatName"
                placeholder="Input group example" aria-label="Input group example" aria-describedby="basic-addon1"
--- frontend/components/MessageArea.vue
+++ frontend/components/MessageArea.vue
@@ -10,11 +10,7 @@
          v-if="chatActive">
       <div class="chat-info-header d-flex align-items-center">
         <a href="#" class="h2" @click="exitThisChat">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left"
-               viewBox="0 0 16 16">
-            <path fill-rule="evenodd"
-                  d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
-          </svg>
+          <icon-arrow-left/>
         </a>
         <a href="#" class="m-2" @click="() => toggleOpenChatConfig()">
           <profile-image :chat-id="chat.id"/>
@@ -54,7 +50,7 @@ const chatActive = computed(() => props.chat.chatId > 0)
 const moreMessagesPath = computed(() =>
     useRuntimeConfig().public.api.getMessagesByIdAndPage.replace(":chatId", props.chat.chatId).replace(":page", store.nextPage).replace(":size", store.pageSize))
 
-const {data: response, refresh } = await useFetch(moreMessagesPath)
+const {data: response, refresh} = await useFetch(moreMessagesPath)
 
 const content = computed(() => {
   return response?.value?.content ?? []
