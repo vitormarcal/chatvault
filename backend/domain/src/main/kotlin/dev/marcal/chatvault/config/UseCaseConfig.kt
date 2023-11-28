43603cd48378f94223bab55007fdebed964c1775
Vitor Marçal <codehero@marcal.dev>
2023-11-28 20:40:12 -0300

    log custom local datetitme pattern (#90)

--- backend/domain/src/main/kotlin/dev/marcal/chatvault/config/UseCaseConfig.kt
+++ backend/domain/src/main/kotlin/dev/marcal/chatvault/config/UseCaseConfig.kt
@@ -1,6 +1,7 @@
 package dev.marcal.chatvault.config
 
 import dev.marcal.chatvault.model.MessageParser
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,10 +13,12 @@ class UseCaseConfig(
     @Value("\${chatvault.msgparser.dateformat}")  private val localDateTimePattern: String?
 ) {
 
+    private val logger = LoggerFactory.getLogger(this.javaClass)
+
 
     @Bean
     fun messageParser(): MessageParser {
-        val customPattern = localDateTimePattern?.takeIf { it.isNotBlank() }
+        val customPattern = localDateTimePattern?.takeIf { it.isNotBlank() }?.also { logger.info("Custom local datetime pattern $localDateTimePattern message parser") }
         return MessageParser(customPattern)
     }
 }
